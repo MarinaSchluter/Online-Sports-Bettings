8e26e26b890cae019df76c615966ff4c15e4bfe3
Marina-Schluter <161477794+Marina-Schluter@users.noreply.github.com>
2024-04-27 22:25:36 +0200

    Update app.py

--- app.py
+++ app.py
@@ -5,17 +5,10 @@ import pandas as pd
 import base64
 import plotly.express as px
 
-
 import urllib.request
 import json
+import os
 import ssl
-ssl._create_default_https_context = ssl._create_stdlib_context
-
-import numpy as np
-import matplotlib.pyplot as plt
-import seaborn as sns
-import plotly.graph_objects as go
-import joblib
 
 
 # -----------------SITE CONFIGURATION----------------#
@@ -53,8 +46,10 @@ set_background('Images/footballstadiumtransparent.png', horizontal_offset=100, v
 
 # ---------------------DATAFRAMES-----------------------------#
 
-mysportsbetting=pd.read_csv("Data\mysportsbetting.csv")
 online_betting_houses=pd.read_csv("Data\online_betting_houses.csv")
+mysportsbetting=pd.read_csv("Data\mysportsbetting.csv")
+mysportsbetting_filtered=pd.read_csv("Data\mysportsbetting_filtered.csv")
+sports_regression=pd.read_csv("Data\sports_regression.csv")
 
 # ---------------------HEADER & LOGO-----------------------------#
 
@@ -90,7 +85,169 @@ with tab1:
 
 #-----TAB 2 (EVENTS DISTRIBUTION)------#
 
+with tab2:
+        
+    col1,col2,col3,col4,col5=st.columns(5)
+    with col1:
+        SPORT_G_OPTIONS=sorted(mysportsbetting["Sport_Group"].unique())
+        sportg= st.multiselect("Select Sport Group:",SPORT_G_OPTIONS)
+        if sportg:
+            mysportsbetting=mysportsbetting[mysportsbetting["Sport_Group"].isin(sportg)]
+    with col2:
+        PURCHASE_T_OPTIONS=["Prematch","Live"]
+        purchase = st.multiselect("Select Purchase Time:", PURCHASE_T_OPTIONS)
+        if purchase:
+            mysportsbetting=mysportsbetting[mysportsbetting["Purchase_Time"].isin(purchase)]
+    with col3:
+        GENDER_OPTIONS=sorted(mysportsbetting["Gender_Competition"].unique())
+        gender= st.multiselect("Select Gender:",GENDER_OPTIONS)
+        if gender:
+            mysportsbetting=mysportsbetting[mysportsbetting["Gender_Competition"].isin(gender)]
+    with col4:
+        MONTH_OPTIONS=["January","February","March"]
+        month= st.multiselect("Select Month:",MONTH_OPTIONS)
+        if month:
+            mysportsbetting=mysportsbetting[mysportsbetting["Month_Name"].isin(month)]
+    with col5:
+        WEEKDAY_OPTIONS=["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
+        weekday = st.multiselect("Select Weekday:", WEEKDAY_OPTIONS)
+        if weekday:
+            mysportsbetting=mysportsbetting[mysportsbetting["Day_of_Week_Name"].isin(weekday)]
+        
+    competitions_by_date = mysportsbetting.groupby("Event_Date")["Competition"].count().reset_index()
+    fig = px.line(competitions_by_date, x="Event_Date", y="Competition", title="Total Competitions by Event Date",width=1750,height=300 )
+    fig.update_xaxes(title="Date")
+    fig.update_yaxes(title="Number of Events")
+    st.plotly_chart(fig)
+    
+    col1,col2=st.columns(2)
+    color_map_sport_group = {                                        #Create a color map representative of each Sport_Group for quick association and visualisation of elements
+    "Football": "darkgreen",
+    "Basketball": "red",
+    "Greyhound Racing": "lightgrey",
+    "Horse Racing": "burlywood",
+    "Tennis": "lime",
+    "Other": "lightblue"  
+}
+    color_map_sports = {
+    "Basketball": "red",
+    "Handball": "darkblue",
+    "Bandy": "dimgray",
+    "Baseball": "saddlebrown",
+    "Boxing": "gold",
+    "Horse Racing": "burlywood",
+    "Cycling": "royalblue",
+    "Cricket": "darkgreen",
+    "Darts": "darkred",
+    "Winter Sports": "lightblue",
+    "Formula 1": "firebrick",
+    "Football": "darkgreen",
+    "American Football": "darkslategray",
+    "Indoor Football": "green",
+    "Greyhound Racing": "lightgrey",
+    "Golf": "limegreen",
+    "Ice Hockey": "lightsteelblue",
+    "Lotto": "gold",
+    "MMA": "darkslategray",
+    "Padel": "midnightblue",
+    "Rugby Union": "royalblue",
+    "Snooker": "mediumseagreen",
+    "Squash": "darkorange",
+    "Tennis": "lime",
+    "Table Tennis": "skyblue",
+    "Volleyball": "lightcoral",
+    "Beach Volleyball": "lightyellow",
+    "Waterpolo": "dodgerblue"
+}
+    with col1:
+            fig=px.histogram(mysportsbetting, x="Sport_Group", title="Event Distribution by Sport Group", 
+                             color="Sport_Group",color_discrete_map=color_map_sport_group,text_auto=True,
+                             category_orders={"Sport_Group": mysportsbetting["Sport_Group"].value_counts().index},height=300)
+            fig.update_xaxes(title="Sport Group")
+            fig.update_yaxes(title="Number of Events")
+            st.plotly_chart(fig)
+    with col2:
+        other_sports=mysportsbetting[mysportsbetting["Sport_Group"]=="Other"]
+        fig=px.histogram(other_sports, x="Sport", title="Other Sports Event Distribution", color="Sport",
+                 color_discrete_map=color_map_sports,text_auto=True,
+                 category_orders={"Sport": mysportsbetting["Sport"].value_counts().index},height=300)
+        fig.update_xaxes(title="Sport")
+        fig.update_yaxes(title="Number of Events")
+        st.plotly_chart(fig)   
+        
+    col1,col2=st.columns([3,3])    
+    color_map_purchase_time = {                                 #Create a color map different from the ones already used in Sports and Sport_Group
+    "Live": "gold",
+    "Prematch": "mediumpurple"
+}
+    color_map_gender = {
+    'Male': 'cornflowerblue',
+    'Female': 'lightpink',
+    'Not Applicable': 'lightgrey'
+}
+    with col1:
+        fig=px.histogram(mysportsbetting, x="Purchase_Time", title="Events Distribution by Purchase Time",
+                    color="Purchase_Time",color_discrete_map=color_map_purchase_time,text_auto=True,height=300)
+        fig.update_xaxes(title="Purchase Time")
+        fig.update_yaxes(title="Number of Events")
+        st.plotly_chart(fig)  
+    with col2:
+        fig=px.histogram(mysportsbetting, x="Gender_Competition", title="Events Distribution by Gender",
+                 color="Gender_Competition",color_discrete_map=color_map_gender,text_auto=True,height=300)
+        fig.update_xaxes(title="Gender")
+        fig.update_yaxes(title="Number of Events")
+        st.plotly_chart(fig)  
+    col1,col2=st.columns([3,3])
+    with col1:
+        ordered_month_names = ["January", "February", "March"]
+        fig=px.histogram(mysportsbetting, x="Month_Name", title="Event Distribution by Month",category_orders={"Month_Name":ordered_month_names}, 
+                         color="Month",text_auto=True,height=300)
+        fig.update_xaxes(title="Month")
+        fig.update_yaxes(title="Number of Events")
+        st.plotly_chart(fig)  
+    with col2:
+        ordered_weekday_names = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
+        fig=px.histogram(mysportsbetting, x="Day_of_Week_Name", title="Event Distribution by Day of the Week",category_orders={"Day_of_Week_Name":ordered_weekday_names},
+                         color="Day_of_Week",text_auto=True,height=300)
+        fig.update_xaxes(title="Weekday")
+        fig.update_yaxes(title="Number of Events")
+        st.plotly_chart(fig)  
+
 #-----TAB 3 (WAGER VS WIN)------#
+
+
+with tab3:
+    col1,col2,col3=st.columns(3)
+    with col1:
+        SPORTS=['Basketball', 'Other', 'Horse Racing', 'Football', 'Greyhound Racing', 'Tennis']
+        sport= st.multiselect("Select Sport Group:",SPORTS)
+        if sport:
+            mysportsbetting=mysportsbetting[mysportsbetting["Sport_Group"].isin(sport)]
+    with col2:
+        PURCHASE=["Live","Prematch"]
+        purchaset = st.multiselect("Select Purchase Time:", PURCHASE)
+        if purchaset:
+            mysportsbetting=mysportsbetting[mysportsbetting["Purchase_Time"].isin(purchaset)]
+    with col3:
+        GENDER=['Male', 'Female', 'Not Applicable']
+        genderc= st.multiselect("Select Gender:",GENDER)
+        if genderc:
+            mysportsbetting=mysportsbetting[mysportsbetting["Gender_Competition"].isin(genderc)]
+            
+    data_by_date = mysportsbetting.groupby("Event_Date").agg({"Wager": "sum", "Winnings": "sum"}).reset_index()
+    fig = px.line(data_by_date, x="Event_Date", y=["Wager", "Winnings"], title="Evolution of Wager and Winnings by Date",
+                labels={"Event_Date": "Date", "value": "Amount", "variable": "Type"},width=1750,height=400)
+    st.plotly_chart(fig)
+    
+    # data_by_weekday = mysportsbetting.groupby("Day_of_Week_Name").agg({"Wager": "sum", "Winnings": "sum"}).reset_index()
+    # fig = px.line(data_by_weekday, x="Day_of_Week_Name", y=["Wager", "Winnings"], title="Evolution of Wager and Winnings by Weekday",
+    #             labels={"Day_of_Week_Name": "Weekday", "value": "Amount", "variable": "Type"},width=1750,height=300)
+    # st.plotly_chart(fig)
+    
+    data_by_sport = mysportsbetting.groupby("Sport_Group").agg({"Wager": "sum", "Winnings": "sum"}).reset_index()
+    fig = px.scatter(data_by_sport, x="Sport_Group", y=["Wager", "Winnings"], title="Evolution of Wager and Winnings by Sport Group",
+                labels={"Sport_Group": "Sport Group", "value": "Amount", "variable": "Type"},width=1750,height=400)
+    st.plotly_chart(fig)
 
 #-----TAB 4 (POWERBI)------#
 
@@ -104,52 +261,73 @@ with tab4:
 #-----TAB 5 (PREDICTION)------#
 
 with tab5:
-# API_KEY = 'ZafUdLi6NvawUl62GVALTCKe8AluIo7F'
-# URL = 'https://machinelearningarea-cbbjm.francecentral.inference.ml.azure.com/score'
+    
+    st.markdown("**Equivalence Tables**:")
+    col1,col2,col3=st.columns(3)
+    with col1:
+        purchase_time_mapping = sports_regression.drop_duplicates(subset=["Purchase_Time_encoded"])[["Purchase_Time_encoded", "Purchase_Time"]]
+        purchase_time_mapping=purchase_time_mapping.sort_values(by="Purchase_Time_encoded")
+        purchase_time_mapping_html=purchase_time_mapping.to_html(index=False)
+        st.write(purchase_time_mapping_html, unsafe_allow_html=True)
+    with col2:
+        sport_group_mapping = sports_regression.drop_duplicates(subset=["Sport_Group_encoded"])[["Sport_Group_encoded", "Sport_Group"]]
+        sport_group_mapping = sport_group_mapping.sort_values(by="Sport_Group_encoded")
+        sport_group_mapping_html=sport_group_mapping.to_html(index=False)
+        st.write(sport_group_mapping_html, unsafe_allow_html=True)
+    with col3:
+        st.markdown("")
+    st.markdown("")
+        
+    ssl._create_default_https_context = ssl._create_stdlib_context
 
-    st.title("Profit Prediction by Wager, Sport Group & Purchase Time")
 
+    API_KEY = 'Fdnddx2a9dv4slCnb1Sd6RMSIlUqRkSd'
 
-# minimum_capacity = st.number_input("Número de personas:", min_value=1, value=1)
-# bedrooms = st.number_input("Número de habitaciones:", min_value=1, value=1)
-# bathrooms = st.number_input("Número de baños:", min_value=1, value=1)
-# bathroom_type = st.selectbox("Baño compartido:", SHARED_BATHROOMS_OPTIONS)
-# ratings = st.number_input("Valoración:", min_value=1, value=1)
-# ratings = st.selectbox("Valoración:", list(range(1,5)),index=0)
+    URL = 'https://marina-zzkge.francecentral.inference.ml.azure.com/score'
 
-# if st.button("Realizar predicción"):
-#     # Estructura de datos para la solicitud POST
-#     data = {
-#         "input_data": {
-#         "columns": [
-#         "neighbourhood_group",
-#         "room_type",
-#         "accommodates",
-#         "bedrooms"
-#             ],
-#             "index": [0],
-#             "data": [[neighbourhood_group,room_type,minimum_capacity,bedrooms]]
-#         }
-#     }
+    Purchase_Time_encoded, Sport_Group_encoded, Wager = st.columns(3)
+    with Purchase_Time_encoded:
+        Purchase_Time_encoded = st.number_input("Select/Enter Purchase Time:", min_value=0, value=1)
 
-#     # Convertir a JSON
-#     body = str.encode(json.dumps(data))
+    with Sport_Group_encoded:
+        Sport_Group_encoded = st.number_input("Select/Enter Sport Group:", min_value=0, value=1)
 
-#     headers = {
-#         'Content-Type': 'application/json',
-#         'Authorization': ('Bearer ' + API_KEY)
-#     }
+    with Wager:
+        Wager = st.number_input("Select/Enter Wager amount:", min_value=0, value=1)
 
-#     # Crear y enviar la solicitud POST
-#     try:
-#         req = urllib.request.Request(URL, body, headers)
-#         with urllib.request.urlopen(req) as response:
-#             result = json.loads(response.read())
-#             predicted_price = round(result[0], 2)
-#             st.write("Precio predicho:")
-#             st.markdown(f"### Precio predicho: **${predicted_price}**")
-            
-#     except urllib.error.HTTPError as error:
-#         st.error(f"Error en la solicitud: {error.code}")
-#         st.write(error.info())
-#         st.write(error.read().decode("utf8", 'ignore'))
+    if st.button("Perform prediction"):
+        # Estructura de datos para la solicitud POST
+        data = {
+            "input_data": {
+            "columns": [
+            "Purchase_Time_encoded",
+            "Sport_Group_encoded",
+            "Wager"
+                ],
+                "index": [0],
+                "data": [[Purchase_Time_encoded,Sport_Group_encoded,Wager]]
+
+            }
+        }
+
+        # Convertir a JSON
+        body = str.encode(json.dumps(data))
+
+        headers = {
+            'Content-Type': 'application/json',
+            'Authorization': ('Bearer ' + API_KEY)
+        }
+
+        # Crear y enviar la solicitud POST
+        try:
+            req = urllib.request.Request(URL, body, headers)
+            with urllib.request.urlopen(req) as response:
+                result = json.loads(response.read())
+                predicted_profit = round(result[0], 2)
+                st.write("Pedicted Profit:")
+                st.markdown(f"### Predicted Profit: **${predicted_profit}**")
+                
+        except urllib.error.HTTPError as error:
+            st.error(f"Application error: {error.code}")
+            st.write(error.info())
+            st.write(error.read().decode("utf8", 'ignore'))    
